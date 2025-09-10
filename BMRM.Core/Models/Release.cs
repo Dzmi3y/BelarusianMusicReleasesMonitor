e3aeb5e7c808c048a8110bc79a8444746af3cad3
Dzmitry <dzmi3y@gmail.com>
2025-09-11 00:39:50 +0300

    Added ReleaseTextParserService.cs

---
+++ BMRM.Core/Models/Release.cs
@@ -0,0 +1,14 @@
+﻿using BMRM.Core.Enums;
+
+namespace BMRM.Core.Models;
+
+public class Release
+{
+    public Guid Id { get; set; }
+    public string? Artist { get; set; }
+    public string? Title { get; set; }
+    public ReleaseType? Type { get; set; }
+    public DateTime? ReleaseDate { get; set; }
+    public string? Genres { get; set; }
+    public string? City { get; set; }
+}
