7060088414db2837ce905ac848ebee11745a2290
Dzmitry <dzmi3y@gmail.com>
2025-09-16 18:06:13 +0300

    Added tray icon

--- BMRM.Desktop/App.xaml.cs
+++ BMRM.Desktop/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Prism.Ioc;
 using BMRM.Desktop.Views;
 using System.Windows;
+using System.Windows.Forms;
+using System.Drawing; 
 
 namespace BMRM.Desktop
 {
@@ -16,6 +18,38 @@ namespace BMRM.Desktop
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
+        }
+        
+        private NotifyIcon _notifyIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            InitializeTrayIcon();
+        }
+
+        private void InitializeTrayIcon()
+        {
+            _notifyIcon = new NotifyIcon
+            {
+                Icon = new Icon("Resources/favicon.ico"),
+                Visible = true,
+                Text = "Belarusian Music Releases Monitor"
+            };
+
+            _notifyIcon.DoubleClick += (s, args) =>
+            {
+                Current.MainWindow.Show();
+                Current.MainWindow.WindowState = WindowState.Normal;
+            };
+
+            _notifyIcon.ContextMenuStrip = new ContextMenuStrip();
+            _notifyIcon.ContextMenuStrip.Items.Add("Выход", null, (s, args) =>
+            {
+                _notifyIcon.Visible = false;
+                _notifyIcon.Dispose();
+                Current.Shutdown();
+            });
         }
     }
 }
--- BMRM.Desktop/ViewModels/MainWindowViewModel.cs
+++ BMRM.Desktop/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ namespace BMRM.Desktop.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "BMRM";
 
         public string Title
         {
