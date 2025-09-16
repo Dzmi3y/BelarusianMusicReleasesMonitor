f71c98b842cb0ec56df3060f81412fefd0f06755
Dzmitry <dzmi3y@gmail.com>
2025-09-17 00:37:31 +0300

    Added db to desktop app

--- BMRM.Desktop/App.xaml.cs
+++ BMRM.Desktop/App.xaml.cs
@@ -1,8 +1,14 @@
-﻿using Prism.Ioc;
+﻿using System;
+using Prism.Ioc;
 using BMRM.Desktop.Views;
 using System.Windows;
 using System.Windows.Forms;
-using System.Drawing; 
+using System.Drawing;
+using System.IO;
+using BMRM.Infrastructure.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace BMRM.Desktop
 {
@@ -11,6 +17,8 @@ namespace BMRM.Desktop
     /// </summary>
     public partial class App
     {
+        public IConfiguration Configuration { get; private set; }
+
         protected override Window CreateShell()
         {
             return Container.Resolve<MainWindow>();
@@ -18,12 +26,44 @@ namespace BMRM.Desktop
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
+            RegisterDB(containerRegistry);
+
+            //containerRegistry.Register<IMusicRepository, MusicRepository>();
         }
-        
+
+        private void RegisterDB(IContainerRegistry containerRegistry)
+        {
+            var connectionString = Configuration.GetConnectionString("Default");
+            
+            var builder = new SqliteConnectionStringBuilder(connectionString);
+            var dbPath = builder.DataSource;
+
+            var directory = Path.GetDirectoryName(dbPath);
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlite(connectionString, x => x.MigrationsAssembly("BMRM.Infrastructure"))
+                .Options;
+
+            var dbContext = new AppDbContext(options);
+
+
+            dbContext.Database.Migrate();
+
+            containerRegistry.RegisterInstance(dbContext);
+        }
+
         private NotifyIcon _notifyIcon;
 
         protected override void OnStartup(StartupEventArgs e)
         {
+            string basePath = AppContext.BaseDirectory;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
             base.OnStartup(e);
             InitializeTrayIcon();
         }
--- BMRM.Desktop/ViewModels/MainWindowViewModel.cs
+++ BMRM.Desktop/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,20 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using BMRM.Core.Models;
+using BMRM.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace BMRM.Desktop.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
+        private AppDbContext _appDbContext;
         private string _title = "BMRM";
+        public ICommand AddTrackCommand { get; }
+        public ICommand InitCommand { get; }
 
         public string Title
         {
@@ -12,7 +22,31 @@ namespace BMRM.Desktop.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
-        public MainWindowViewModel()
+        private int _count = 0;
+
+        public int Count
+        {
+            get { return _count; }
+            set { SetProperty(ref _count, value); }
+        }
+
+        public ObservableCollection<Release> Tracks { get; } = new();
+
+        public MainWindowViewModel(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+            AddTrackCommand = new DelegateCommand(OnAddTrack);
+            InitCommand = new AsyncDelegateCommand(InitAsync);
+        }
+
+        public async Task InitAsync()
+        {
+            var trackList = await _appDbContext.Releases.AsNoTracking().ToListAsync();
+            foreach (var track in trackList)
+                Tracks.Add(track);
+        }
+
+        private void OnAddTrack()
         {
         }
     }
