4cca8965c06e0ff8243e011db5c8b1a97a323400
Dzmitry <dzmi3y@gmail.com>
2025-09-18 21:08:22 +0300

    Added SpotifySearchService, SpotifyPlaylistsService and SpotifyResponseModels

---
+++ BMRM.Infrastructure/Migrations/20250918180248_Added ProcessingStatus and SpotifyAlbumId to Release.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BMRM.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedProcessingStatusandSpotifyAlbumIdtoRelease : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProcessingStatus",
+                table: "Releases",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SpotifyAlbumId",
+                table: "Releases",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProcessingStatus",
+                table: "Releases");
+
+            migrationBuilder.DropColumn(
+                name: "SpotifyAlbumId",
+                table: "Releases");
+        }
+    }
+}
