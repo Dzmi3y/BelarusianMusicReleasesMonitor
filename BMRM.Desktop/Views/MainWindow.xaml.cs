c8a8eaee1813f5c18df9fd8b63f528ca2c39da82
Dzmitry <dzmi3y@gmail.com>
2025-09-19 12:08:17 +0300

    Added ShellControl

--- BMRM.Desktop/Views/MainWindow.xaml.cs
+++ BMRM.Desktop/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 using BMRM.Desktop.ViewModels;
 
 namespace BMRM.Desktop.Views
---
+++ BMRM.Desktop/Views/Shell/ShellControl.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace BMRM.Desktop.Views.Shell
+{
+    public partial class ShellControl : UserControl
+    {
+        public ShellControl()
+        {
+            InitializeComponent();
+        }
+        
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(ShellControl), new PropertyMetadata(""));
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+        
+        
+        public static readonly DependencyProperty ContentProperty =
+            DependencyProperty.Register("Content", typeof(object), typeof(ShellControl),
+                new PropertyMetadata(null, OnContentChanged));
+
+        public new object Content
+        {
+            get => GetValue(ContentProperty);
+            set => SetValue(ContentProperty, value);
+        }
+
+        private static void OnContentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ShellControl shell && shell.PART_ContentHost != null)
+            {
+                shell.PART_ContentHost.Content = e.NewValue;
+            }
+        }
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                var window = Window.GetWindow(this);
+                window?.DragMove();
+            }
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            var window = Window.GetWindow(this);
+            if (window != null)
+                window.WindowState = WindowState.Minimized;
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            var window = Window.GetWindow(this);
+            if (window != null)
+                window.WindowState = window.WindowState == WindowState.Maximized
+                    ? WindowState.Normal
+                    : WindowState.Maximized;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            var window = Window.GetWindow(this);
+            window?.Close();
+        }
+    }
+}
